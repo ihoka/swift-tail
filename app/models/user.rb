9ee4800ff2763277d5686c1fa694dab2abbe7fe9
Istvan Hoka <istvan.hoka@gmail.com>
2025-07-25 11:28:05 +0100

    Fix lint errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,5 @@ class User < ApplicationRecord
   validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :password, presence: true, length: { minimum: 6 }, on: :create
 
-  normalizes :email, with: -> e { e.strip.downcase }
+  normalizes :email, with: ->(e) { e.strip.downcase }
 end
